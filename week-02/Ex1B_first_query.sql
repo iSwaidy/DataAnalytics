bc41b045bebc056c71b040e4ea5e719bba16abac
iSwaidy <99csantamaria@gmail.com>
2026-04-14 10:42:15 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
    Sakila is a database used to collect information about films and rentals for use by a DVD rental store.
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
