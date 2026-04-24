8d3ad500a31d7f3753f81bcecaf086cf840a0822
iSwaidy <99csantamaria@gmail.com>
2026-04-24 01:55:45 -0400

    Ex. 1C Updated

--- week-03/1C_query_practice.sql
+++ week-03/1C_query_practice.sql
@@ -3,57 +3,58 @@ Exercise 1C - Week 03
 Cris Ramirez
 */
 
-
 USE northwind;
 
--- 1. 
-SELECT p.ProductID, p.ProductName, p.UnitPrice, c.CategoryName
-FROM products p
-JOIN categories c 
-	ON p.CategoryID = c.CategoryID
-ORDER BY c.CategoryName, p.ProductName;
+-- 1. Write a query to list the product id, product name, and unit price of every product. 
+-- This time, display them in ascending order by price.
 
--- 2. 
-	SELECT p.ProductID, p.ProductName, p.UnitPrice, s.CompanyName AS SupplierName
-	FROM products p
-	JOIN suppliers s 
-		ON p.SupplierID = s.SupplierID
-	WHERE p.UnitPrice > 75
-	ORDER BY p.ProductName;
+	SELECT ProductID, ProductName, UnitPrice
+	FROM products
+	ORDER BY UnitPrice ASC;
 
--- 3. 
-	SELECT p.ProductID, p.ProductName, p.UnitPrice, 
-       c.CategoryName, s.CompanyName AS SupplierName
-	FROM products p
-	JOIN categories c 
-		ON p.CategoryID = c.CategoryID
-	JOIN suppliers s 
-		ON p.SupplierID = s.SupplierID
-	ORDER BY p.ProductName;
+-- 2. What are the products that we carry where we have at least 100 units on hand? 
+-- Order them in descending order by price.
 
--- 4. 
-	SELECT o.OrderID, o.ShipName, o.ShipAddress, sh.CompanyName AS Shipper
-	FROM orders o
-	JOIN shippers sh 
-		ON o.ShipVia = sh.ShipperID
-	WHERE o.ShipCountry = 'Germany'
-	ORDER BY sh.CompanyName, o.ShipName;
+	SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+	FROM products
+	WHERE UnitsInStock >= 100
+	ORDER BY UnitPrice DESC;
 
--- 5. 
-	SELECT o.ShipName, o.ShipAddress, sh.CompanyName AS Shipper, 
-       COUNT(*) AS OrderCount
-	FROM orders o
-	JOIN shippers sh 
-		ON o.ShipVia = sh.ShipperID
-		WHERE o.ShipCountry = 'Germany'
-	GROUP BY o.ShipName, o.ShipAddress, sh.CompanyName
-	ORDER BY sh.CompanyName, o.ShipName;
+-- 3. What are the products that we carry where we have at least 100 units on hand?
 
--- 6. 
-	SELECT o.OrderID, o.OrderDate, o.ShipName, o.ShipAddress
-	FROM orders o
-	JOIN `order details` od 
-		ON o.OrderID = od.OrderID
-	JOIN products p 
-		ON od.ProductID = p.ProductID
-	WHERE p.ProductName = 'Sasquatch Ale';
+	SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+	FROM products
+	WHERE UnitsInStock >= 100
+	ORDER BY UnitPrice DESC, ProductName ASC;
+
+-- 4. write a query against the orders table that displays the total number of distinct customers who have placed orders, based on customer ID.
+
+	SELECT COUNT(DISTINCT CustomerID) AS CustomerCount
+	FROM orders;
+
+-- 5. Write a query against the orders table that displays the total number of distinct customers who have placed orders, by customer ID, for each country where orders have been shipped. 
+
+	SELECT ShipCountry, COUNT(DISTINCT CustomerID) AS CustomerCount
+	FROM orders
+	GROUP BY ShipCountry
+	ORDER BY CustomerCount DESC;
+
+-- 6. Write a query to list each of the job titles in employees, along with a count of how many employees hold each job title.
+	
+	SELECT ProductID, ProductName, UnitsInStock, UnitsOnOrder
+	FROM products
+	WHERE UnitsInStock < 25 AND UnitsOnOrder >= 1
+	ORDER BY UnitsOnOrder DESC, ProductName ASC;
+
+-- 7. Write a query to list each of the job titles in employees, along with a count of how many employees hold each job title.
+
+	SELECT Title, COUNT(*) AS EmployeeCount
+	FROM employees
+	GROUP BY Title;
+    
+-- 8. What employees have a monthly salary that is between $2000 and $2500? Write a query that orders them by job title.
+
+	SELECT EmployeeID, FirstName, LastName, Title, Salary
+	FROM employees
+	WHERE Salary BETWEEN 2000 AND 2500
+	ORDER BY Title;
