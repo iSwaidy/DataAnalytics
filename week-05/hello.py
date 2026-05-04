6d94a28c862416a0e074cd2841b9b329f4a19355
iSwaidy <99csantamaria@gmail.com>
2026-05-04 11:35:24 -0400

    Python file

--- week-05/hello.py
+++ week-05/hello.py
@@ -1,1 +1,27 @@
+## Cris Ramirez
+## 05/04/26
+## Ex1A_hello.py
+
 print('Hello world!')
+
+message = 'Hello World!'
+print(message)
+
+# Hello world! prints twice because im running two separate print statements
+# the first prints the string directly, the second
+# prints the value stored in the variable message
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+
+# Observation: Python adds them as numbers, not as currency.
+
+cents = cents + .25
+print(dollars + cents)
+
+# Combining strings
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
